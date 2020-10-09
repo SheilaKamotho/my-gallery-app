7b9a9f92f6a340ab614309c0db146dc00b8ceb0b
Kamotho Sheila <kamothosheila@gmail.com>
2020-10-09 14:51:09 +0300

    register classes in admin.py

--- photos/views.py
+++ photos/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http  import HttpResponse
 
 # Create your views here.
-def welcome(request):
+def photos(request):
     return render(request, 'photos.html')
 
 
