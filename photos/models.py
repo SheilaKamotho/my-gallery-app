dbc7cce16b57370231ed686f0d9639f888b08d9b
Kamotho Sheila <kamothosheila@gmail.com>
2020-10-09 15:51:53 +0300

    add attribute changes to model.py

--- photos/models.py
+++ photos/models.py
@@ -31,7 +31,7 @@ class Image(models.Model):
     Class that generates new instance of images
     """
     image_name=models.CharField(max_length=30)
-    image_description=models.CharField(max_length=1000)
+    image_description=models.TextField()
     image_location = models.ForeignKey(Location,on_delete=models.CASCADE, null=True)
     image_category = models.ForeignKey(Category,on_delete=models.CASCADE, null=True)
     post_date = models.DateTimeField(auto_now_add=True)
